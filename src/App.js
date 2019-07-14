9c7212eacc44476586a05f59c6ac833ff7f8282b
Jhonatan R. Saumeth <jhriverasa@unal.edu.co>
2019-07-14 04:12:43 -0500

    refactor filesystem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import NavigationBar from './components/NavigationBar';
-import Footer from './components/Footer';
-import Pieces from './components/Pieces';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import Footer from './components/Footer/Footer';
+import Pieces from './components/Views/Heroes/Pieces';
 
 function App() {
   return (
